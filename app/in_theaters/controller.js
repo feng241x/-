818a84f558ce5dc54bcddf13d4c8a3a57ccaf7b2
feng241x <feng241x@qq.com>
2017-07-22 14:15:03 +0800

    03-实现正在热映模块-分页功能

--- app/in_theaters/controller.js
+++ app/in_theaters/controller.js
@@ -1,22 +1,43 @@
-(function(angular){
+(function (angular) {
   'use strict'
   angular
-    .module('moviecat.in_theaters',[])
-    .config(['$routeProvider',function($routeProvider){
+    .module('moviecat.in_theaters', [])
+    .config(['$routeProvider', function ($routeProvider) {
       $routeProvider
-        .when('/in_theaters',{
-          templateUrl:'./in_theaters/view.html',
-          controller:'in_Controller'
+        .when('/in_theaters/:curPage?', {
+          templateUrl: './in_theaters/view.html',
+          controller: 'in_Controller'
         })
     }])
-    .controller('in_Controller',['$scope','jsonpSrv',function($scope,jsonpSrv){
+    .controller('in_Controller', ['$scope', '$routeParams', '$route', 'jsonpSrv', function ($scope, $routeParams, $route, jsonpSrv) {
+      // 实现分页功能
+      var PAGESIZE = 5,
+        page = 0; //当前页
+      page = $routeParams.curPage || 1;
+      $scope.page = page;
+      var paramObj = {
+        start: (page - 1) * PAGESIZE,
+        count: PAGESIZE
+      }
+      $scope.goPage = function (page) {
+        if (page < 1 || page > $scope.MaxPage) {
+          console.log(123);
+          return;
+        }
+        console.log(page);
+        $route.updateParams({ curPage: page })
+      }
+      // 当前页开始索引 = (当前页 - 1) * 每页数量
+      // start = (page - 1) * PAGESIZE
+
       var url = 'https://api.douban.com/v2/movie/in_theaters';
-      jsonpSrv.jsonp(url,{},function(data){
+      jsonpSrv.jsonp(url, paramObj, function (data) {
         console.log(data);
         $scope.data = data;
+        $scope.MaxPage = Math.ceil(data.total / PAGESIZE)
         // 非angular自身的代码 不会触发脏值检查机制 需要手动调用 $apply() 方法 手动触发脏值检查机制
         $scope.$apply();
       })
-      
+
     }])
 })(angular)
